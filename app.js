c9106c5469457aeab330657b6e04b4c6904ef656
Milchess <milch.ks.2992@gmail.com>
2023-04-17 21:43:35 +0500

    Созданы модели и файл app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const { PORT = 3000, CONNECT = 'mongodb://0.0.0.0:27017/bitfilmsdb' } = process.env;
+const app = express();
+
+async function main() {
+  await mongoose.connect(CONNECT);
+  await app.listen(PORT);
+  console.log(`Сервер запущен на ${PORT} порту`);
+}
+
+main();
